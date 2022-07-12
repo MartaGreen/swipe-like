098bbf7a04f7df2bb6b8e2596544394e3bced08b
MartaGreen <iguana13131@gmail.com>
2022-07-12 11:53:43 +0400

    feat: start header development

---
+++ src/header/header.js
@@ -0,0 +1,7 @@
+import "./header.scss";
+
+import "../../assets/images/header/search.svg";
+
+import "../../assets/images/header/cart.svg";
+import "../../assets/images/header/profile.svg";
+import "../../assets/images/header/like.svg";
--- src/index.js
+++ src/index.js
@@ -1,1 +1,6 @@
 import "./index.scss";
+
+// shared
+import "./shared/styles/buttons.scss";
+
+import "./header/header";
