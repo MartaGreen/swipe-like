4cd4ff206d0876016435e2d80b120d8c56fd600f
MartaGreen <iguana13131@gmail.com>
2022-07-15 11:29:10 +0400

    feat: finish program1 page development

--- src/pages/program1/program1.js
+++ src/pages/program1/program1.js
@@ -2,3 +2,6 @@ import "./program1.scss";
 
 import "../../components/exercise/exercise";
 import "../../components/back-btn/back-btn";
+
+import "../../../assets/images/program1-page/exercise1_icon.svg";
+import "../../../assets/images/program1-page/exercise2_icon.svg";
