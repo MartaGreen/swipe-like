c177257f0a7fbe9be92b6f0b0d0417f13ec7165d
MartaGreen <iguana13131@gmail.com>
2022-07-16 15:43:22 +0400

    refactor: create week-table component

--- src/pages/exercise/exercise.js
+++ src/pages/exercise/exercise.js
@@ -1,5 +1,6 @@
 import "./exercise.scss";
 
 import "../../components/back-btn/back-btn";
+import "../../components/week-table/week-table";
 
 import "../../../assets/images/program1-page/exercise1_icon.svg";
