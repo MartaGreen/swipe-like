b7c4f08b300f07ce2764a3a0c41554a14e138e3e
MartaGreen <iguana13131@gmail.com>
2022-08-04 16:50:44 +0400

    fix: change configs

---
+++ src/shared/shared.js
@@ -0,0 +1,9 @@
+import "normalize-scss";
+
+import "./styles/buttons.scss";
+import "./styles/fonts.scss";
+import "./styles/pages.scss";
+import "./styles/reset.scss";
+
+import "../components/header/header";
+import "../components/preloader/preloader";
--- webpack.config.js
+++ webpack.config.js
@@ -1,5 +1,4 @@
 const HtmlWebpackPlugin = require("html-webpack-plugin");
-const { CleanWebpackPlugin } = require("clean-webpack-plugin");
 const HtmlWebpackPugPlugin = require("html-webpack-pug-plugin");
 const path = require("path");
 
@@ -8,33 +7,33 @@ const plugins = [
     title: "Smart platform",
     filename: "index.html",
     template: path.resolve(__dirname, "src", "pages/index", "index.pug"),
-    chunks: ["build", "index"],
+    chunks: ["shared", "index"],
   }),
   new HtmlWebpackPlugin({
     title: "Program 1",
     filename: "program1.html",
     template: path.resolve(__dirname, "src", "pages/program1", "program1.pug"),
-    chunks: ["build", "program1"],
+    chunks: ["shared", "program1"],
   }),
   new HtmlWebpackPlugin({
     title: "Exercise",
     filename: "exercise.html",
     template: path.resolve(__dirname, "src", "pages/exercise", "exercise.pug"),
-    chunks: ["build", "exercise"],
+    chunks: ["shared", "exercise"],
   }),
   new HtmlWebpackPugPlugin(),
-  new CleanWebpackPlugin(),
 ];
 
 module.exports = {
   entry: {
-    build: path.resolve(__dirname, "src", "build.js"),
+    shared: path.resolve(__dirname, "src/shared", "shared.js"),
     index: path.resolve(__dirname, "src", "pages/index", "index.js"),
     program1: path.resolve(__dirname, "src", "pages/program1", "program1.js"),
     exercise: path.resolve(__dirname, "src", "pages/exercise", "exercise.js"),
   },
   output: {
     filename: "[name].[hash].js",
+    clean: true,
   },
 
   module: {
@@ -49,9 +48,16 @@ module.exports = {
       },
       {
         test: /\.(jpe?g|png|svg|gif)$/i,
-        use: {
-          loader: "file-loader",
-          options: { name: "images/[name].[ext]" },
+        type: "asset/resource",
+        generator: {
+          filename: "images/[name][ext]",
+        },
+      },
+      {
+        test: /\.(ttf|woff|woff2|eot)$/i,
+        type: "asset/resource",
+        generator: {
+          filename: "fonts/[name][ext]",
         },
       },
     ],
